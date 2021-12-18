d2f1c76d923d6223a5e9eecaebd8c498f6f54795
Arpan Mondal <arpan19991025@gmail.com>
2021-12-19 00:15:07 +0530

    Add slice reducers to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import authReducer from "./Features/auth/authSlice";
+import userReducer from "./Features/User/userSlice";
 
 export const store = configureStore({
   reducer: {
-
+    auth: authReducer,
+    user: userReducer,
   },
 });
