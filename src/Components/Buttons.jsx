3f0c9175fe65cbb262085ccf9816cad9b96ac7c9
Arpan Mondal <arpan19991025@gmail.com>
2022-01-22 21:48:43 +0530

    Style: Update buttons style

--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -5,7 +5,7 @@ export const Button = ({ children, variant, size, state, ...props }) => {
     <button
       className={`w-max rounded-md shadow-xl ${
         variant === "primary"
-          ? `bg-gradient-to-r from-primary-700 to-pink-600 text-white`
+          ? `bg-gradient-to-r from-primary-700 to-pink-600 text-white shadow-md shadow-pink-500 transition-transform duration-[300ms] hover:-translate-y-[4px]`
           : ``
       }
         ${
@@ -13,8 +13,15 @@ export const Button = ({ children, variant, size, state, ...props }) => {
             ? `border-2 border-solid border-red-400 text-red-500`
             : ``
         }
+        ${
+          variant === "github"
+            ? `bg-gradient-to-r from-[#24292F] to-[#424547] text-white justify-center flex gap-4 items-center shadow-md shadow-[#747677d3] transition-transform duration-[300ms] hover:-translate-y-[4px]`
+            : ``
+        }
+
         ${size === "small" && `py-2 px-4`}
         ${size === "large" && `py-2 px-8`}
+        ${size === "full" && `py-2 px-8 w-full`}
         ${size === "xSmall" && `py-1 px-2`}
         ${state === "fulfilled" && `cursor-not-allowed`}
         `}
