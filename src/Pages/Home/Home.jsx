e5aa5e1860e377b9f876929c027bda01e72ad32e
Arpan Mondal <arpan19991025@gmail.com>
2022-01-25 17:36:20 +0530

    Add header in home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,16 +1,9 @@
-import { Link } from "react-router-dom"
-import { useSelector } from "react-redux"
+import { Header } from "../../Components";
 
 export const Home = () => {
-  const { token } = useSelector(( state ) =>  state.auth )
   return (
-    <div>
-      <h1>Hello from Home page</h1>
-      <Link to="/user">
-        {
-          token ? "Profile" : "Login"
-        }
-      </Link>
+    <div className="w-screen min-h-screen">
+      <Header />
     </div>
   );
 };
