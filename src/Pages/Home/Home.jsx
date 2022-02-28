5a95d06b1df5db56442afc72846ae85d26fa2e5d
Arpan Mondal <arpan19991025@gmail.com>
2022-02-28 11:01:35 +0530

    Update: Add header and post in home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,14 @@
-import { Header } from "../../Components";
+import { Header, CreatePost } from "../../Components";
 
 export const Home = () => {
   return (
     <div className="w-screen min-h-screen">
       <Header />
+      <div className="w-full min-h-screen px-4">
+        <div className="max-w-2xl min-h-screen mx-auto border-x border-primary-700 pt-16">
+          <CreatePost />
+        </div>
+      </div>
     </div>
   );
 };
