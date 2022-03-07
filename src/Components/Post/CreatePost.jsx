ae4e654d179a8dd5758647049758da76e30749dc
Arpan Mondal <arpan19991025@gmail.com>
2022-03-07 16:17:26 +0530

    Add createpost component

--- src/Components/Post/CreatePost.jsx
+++ src/Components/Post/CreatePost.jsx
@@ -1,24 +1,57 @@
 import { ProfileImage } from "../Header/Navigations/Profile/profileImage";
-import { Button } from "../Buttons"
+import { Button } from "../Buttons";
+import { useRef, useState } from "react";
 
 export const CreatePost = () => {
+  const [postValue, setPostValue] = useState([]);
+  const postRef = useRef(null);
+
+  const userInputHandler = (evt) => {
+    if (evt.key === "Enter") {
+      evt.preventDefault();
+    }
+    setPostValue(getLinesOfText(postRef));
+  };
+
+  const getLinesOfText = (postRef) => {
+    const childNodes = postRef.current.childNodes;
+    // Get the text nodes
+    const textNodes = [...childNodes].map((node) => node.textContent);
+    // replace the &nbsp; with space in each word in each line and trim the words
+    const modifiedTextNodes = textNodes.map((line) =>
+      line.replace("&nbsp;", " ").split(/\s+/).join(" ").trim()
+    );
+    //filter out only text and remove breaks
+    const finalTextNodes = modifiedTextNodes.filter((line) => line !== "");
+    return finalTextNodes;
+  };
+
   return (
-    <div className="w-full h-44 rounded-md shadow-lg shadow-pink-300 border border-primary-700 flex pr-4">
-      <div className="h-full w-1/5 flex justify-center py-4">
+    <div className="w-full min-h-[11rem] rounded-md shadow-lg shadow-pink-300 border border-primary-700 flex pr-4">
+      <div className="min-h-[11rem] w-1/5 flex justify-center py-4">
         <ProfileImage
           imageStyles={{ width: "60px", height: "60px", cursor: "pointer" }}
           imageClassNames="rounded-full"
         />
       </div>
-      <div className="h-full w-4/5 flex flex-col">
-        <div className="w-full h-[70%] border-0 border-b border-primary-700">
-          <textarea
-            className="w-full h-full resize-none outline-none p-2 tracking-widest text-gray-800"
+      <div className="min-h-[11rem] w-4/5 flex flex-col">
+        <div className="w-full min-h-[70%] border-0 border-b border-primary-700">
+          <div
+            className="w-full outline-none p-2 tracking-widest text-gray-800 inline-block"
             placeholder="Got something to share?"
-          />
+            onKeyUp={userInputHandler}
+            contentEditable={true}
+            ref={postRef}
+          ></div>
         </div>
-        <div className="w-full h-[30%] flex py-2">
-          <Button variant={"primary"} size={"large"} className="ml-auto">
+        <div className="w-full h-[4rem] flex py-2">
+          <Button
+            variant={"primary"}
+            size={"large"}
+            className={`ml-auto ${
+              postValue.length === 0 && "cursor-not-allowed"
+            }`}
+          >
             Post
           </Button>
         </div>
